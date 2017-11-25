c0521cc4b725e181f4b9b2f55c06adf9984e4956
Sainath Adapa <adapasainath@gmail.com>
2017-11-25 17:57:54 +0530

    Added Gitlab support

--- background.js
+++ background.js
@@ -1,26 +1,21 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
+
     var url_root = 'http://nbviewer.ipython.org/';
     var url = null;
-    var gist_re = /^https?:\/\/gist\.github\.com\/(?:\w+\/)?([a-f0-9]+)$/;
+
     var github_re = /^https:\/\/(github\.com\/.*\/)blob\/(.*\.ipynb)$/;
-    var https_re = /^https:\/\/(.*\.ipynb)$/;
-    var http_re = /^http:\/\/(.*\.ipynb)$/;
+    var gitlab_re = /^https:\/\/(gitlab\.com\/.*\/)blob\/(.*\.ipynb)$/;
+
     var loc = details.url;
     var path;
 
-    if (gist_re.test(loc)) {
-      gist = gist_re.exec(loc);
-      url = url_root + gist[1];
-    } else if (github_re.test(loc)) {
+    if (github_re.test(loc)) {
       path = github_re.exec(loc);
       url = url_root + 'urls/raw.' + path[1] + path[2];
-    } else if (https_re.test(loc)) {
-      path = https_re.exec(loc);
-      url = url_root + 'urls/' + path[1];
-    } else if (http_re.test(loc)) {
-      path = http_re.exec(loc);
-      url = url_root + 'url/' + path[1];
+    } else if (gitlab_re.test(loc)) {
+      path = gitlab_re.exec(loc);
+      url = url_root + 'urls/' + path[1] + 'raw/' + path[2];
     }
 
     if (url) {
@@ -30,7 +25,9 @@ chrome.webRequest.onBeforeRequest.addListener(
   {
     urls: [
       "*://github.com/*",
-      "*://www.github.com/*"
+      "*://www.github.com/*",
+      "*://gitlab.com/*",
+      "*://www.gitlab.com/*"
     ],
     types: ["main_frame", "sub_frame"]
   },
